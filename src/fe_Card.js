cc9c53fd19bda80e218209674f5700aa98371eaa
童一 <tongwenfan1991@yahoo.com>
2021-12-04 16:31:05 +0800

    1

--- src/fe_Card.js
+++ src/fe_Card.js
@@ -1,313 +1,214 @@
-import React, { useEffect, useState, useRef } from "react";
-import { withRouter } from "react-router-dom";
-import Page from "@/components/Layout/Page";
-import SectionTitle from "@/components/SectionTitle";
-import "./index.less";
-import moment from 'moment'
-import { tabsView } from "./business/index";
-import { Button, Tabs, Row, Col, Modal, message, Input, Form } from "antd";
-import Loading from "@/components/Loading";
-import { useSelector } from 'react-redux'
-import {  RightOutlined,DownOutlined,WarningFilled   }  from '@ant-design/icons';
+import React, { useEffect, useState } from 'react'
+import { Row, Col, Card, Input } from 'antd'
+import './App.less'
 import {
-  OKSvg,
-  DelsSvg,
-  SaveSvg,
-} from "../../expense/paymentDetail/business/iconSvg";
-import api from '@/api/itemPacking'
-import { getQueryString } from "@/utils";
-const { TextArea } = Input;
-const { TabPane } = Tabs;
-const PackingDetail = (props) => {
-  useEffect(() => {
-    getDetail()
-    return () => {};
-  }, []);
-  const [visible, setVisible] = useState(false);
-  const [data, setData] = useState({
-    ItemBaseInfoDetail:{},
-    ItemPackingSpecificationDetails:[
-     
-    ],
-    ItemOperationLogDetails:[]
-  });
-  const [logList,setLogList] = useState([])
-  const [checkLog,setCheck] = useState(false) // true = 展开 false = 不展开
-  const [form] = Form.useForm();
-  function callback(key) {
-    console.log(key);
-  }
-
-  const getDetail = async () => {
-   let TransactionNumber = getQueryString("TransactionNumber");
-    const {Code,Data} = await api.detail(TransactionNumber!== undefined ? TransactionNumber : data.ItemBaseInfoDetail.TransactionNumber )
-    if(!Code){
-      const { ItemOperationLogDetails } = Data
-    let _log =   ItemOperationLogDetails.length > 0 ? [ItemOperationLogDetails[0]]  : []
-      setCheck(false)
-      setData({...Data})
-      setLogList(_log)
-    }
-   
-  };
-  const openLog = () => {
-    const { ItemOperationLogDetails } = data
-    let _checkLog = !checkLog
-    let  _log =  _checkLog ? ItemOperationLogDetails :  (  ItemOperationLogDetails.length > 0 ? [ItemOperationLogDetails[0]]  : [] )
-    setLogList(_log)
-    setCheck(_checkLog)
-  }
-  const handleOk = async () => {
-    const searchData = await form.validateFields();
-    const {Code} = await api.reject({
-        TransactionNumber: data.ItemBaseInfoDetail.TransactionNumber,
-        Notice: searchData.Notice
+  StarTwoTone,
+  CheckCircleOutlined,
+  UpSquareOutlined,
+} from '@ant-design/icons'
+const MyCard = ({ title = '2222' }) => {
+  const [data, setData] = useState([])
+  const [data2, setData2] = useState([
+    // isEdit: true,
+    // value: '',
+    // isColor: false,
+    // isCheckAnswer:false
+  ])
+  const [data3, setData3] = useState([])
+  const [data4, setData4] = useState([])
+  // 添加词块
+  const setPushData = () => {
+    data.push({
+      isEdit: true,
+      value: '',
+      isColor: false,
     })
-    Code === 0 && message.success('Successful operation~')
-    getDetail()
-     // 请求详情
-  };
-  const FormatItem = (item) => item ? item :'--'
-  const handleCancel = () => {
-    setVisible(false);
-    form.resetFields();
-  };
-  const rejected = () => setVisible(true);
-  const approve = async () => {
-    const {ItemBaseInfoDetail} = data
-    const {Code} = await api.approve(ItemBaseInfoDetail.TransactionNumber)
-    Code === 0 && message.success('Successful operation~')
-    // 请求详情
-    getDetail()
-  };
-  const stateList = {
-    1:'Draft',
-    2:'Pending',
-    3:'Activated',
-    4:'Deactivated',
+    setData([...data])
   }
-  const enumerateData = useSelector(state => state.enumerate.list)
-  const Item_Packing_Type = enumerateData.find(o => o.Category === 'Item_Packing_Type')?.ItemList || []
-  const Item_OperationLog_Type = enumerateData.find(o => o.Category === 'Item_OperationLog_Type')?.ItemList || []
-  const getTypeName =  (list,_code) => {
-    let _name = ""
-    list.map(item => {  
-      if(item.Code === _code){
-        _name = item.Name
-      }
-     })
-     return _name
+  // 添加干扰
+  const setDisturbData = () => {
+    data4.push({
+      isEdit: true,
+      value: '',
+      isColor: false,
+    })
+    setData4([...data4])
   }
-  const { 
-    ItemBaseInfoDetail,
-  ItemPackingSpecificationDetails,
-  ItemOperationLogDetails
- } = data
-  return (
-    <Page
-    id={
-     `Detail`
-    }
-  >
-      <div className="PackingDetail">
-        <Row gutter={12} className="header">
-          <Col md={12} lg={12} xl={12} xxl={12}>
-            <SectionTitle title="Basic information" />
-            <div className={` header-card  ${stateList[ItemBaseInfoDetail.Status]}-bg`}>
-              <div className="table-status-wrap flex-sc">
-                <span className={`status ${stateList[ItemBaseInfoDetail.Status]}-packing`}></span>
-                <span>{stateList[ItemBaseInfoDetail.Status]}</span>
-              </div>
-            </div>
-          </Col>
-        </Row>
-        <Row gutter={12} className="content">
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">Newegg Item# :</p>
-            <p  className="value">{FormatItem(ItemBaseInfoDetail?.NeweggItemNumber)}</p>
-          </Col>
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">Seller Part# :</p>
-            <p  className="value">{FormatItem(ItemBaseInfoDetail?.ClientItemNumber)}</p>
-          </Col>
-          <Col md={12} lg={12} xl={12} xxl={12}>
-            <p className="label">Client :</p>
-            <p className="value href">{FormatItem(ItemBaseInfoDetail?.ClientName)}</p>
-          </Col>
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">HS Code :</p>
-            <p className="value ">{FormatItem(ItemBaseInfoDetail?.Hscode)}</p>
-          </Col>
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">First Legal Unit :</p>
-            <p className="value ">{FormatItem(ItemBaseInfoDetail?.FirstLegalUnitName)}</p>
-          </Col>
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">Second Legal Unit :</p>
-            <p className="value ">{FormatItem(ItemBaseInfoDetail?.SecondLegalUnitName)}</p>
-          </Col>
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">Inner Packing Qty :</p>
-            <p className="value ">{FormatItem(ItemBaseInfoDetail?.InnerQuantity)}</p>
-          </Col>
-          <Col md={12} lg={12} xl={12} xxl={12}>
-            <p className="label">Item Chinese Name :</p>
-            <p className="value ">{FormatItem(ItemBaseInfoDetail?.ItemChineseName)}</p>
-          </Col>
-          <Col md={12} lg={12} xl={12} xxl={12}>
-            <p className="label">Item English Name :</p>
-            <p className="value ">{FormatItem(ItemBaseInfoDetail?.ItemEnglishName)}</p>
-          </Col>
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">Weight (KG) :</p>
-            <p className="value ">{FormatItem(ItemBaseInfoDetail?.Weight)}</p>
-          </Col>
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">Dimension (mm) :</p>
-            <p className="value ">{FormatItem(`${(ItemBaseInfoDetail?.Length)} * ${ItemBaseInfoDetail?.Width} * ${ItemBaseInfoDetail?.Height}`)}</p>
-          </Col>
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">Currency :</p>
-            <p className="value ">{FormatItem(ItemBaseInfoDetail?.Currency)}</p>
-          </Col>
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">Unit Price :</p>
-            <p className="value ">{FormatItem(ItemBaseInfoDetail?.UnitPrice)}</p>
-          </Col>
 
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">Product Origin : :</p>
-           <p className="value ">{FormatItem(ItemBaseInfoDetail?.ProductOrigin)}</p>
-          </Col>
-          <Col md={12} lg={12} xl={12} xxl={12}>
-            <p className="label">Domestic Source :</p>
-            <p className="value ">{FormatItem(ItemBaseInfoDetail?.DomesticSource)}</p>
-          </Col>
-          <Col md={6} lg={6} xl={6} xxl={6}>
-            <p className="label">Hazard :</p>
-  
-            <p className="value "><WarningFilled  style={{ color: '#F5222D', display: ItemBaseInfoDetail.HazardFlag === 1 ? 'inline':'none' }} /> {ItemBaseInfoDetail?.HazardFlag === 1 ? '危险品' : (ItemBaseInfoDetail?.HazardFlag ===2 ? '非危险品' : '')}</p>
-          </Col>
-        </Row>
-        {/* 图片 */}
-        <p className="label">Item Image :</p>
-        <Row gutter={12} className="content">
-          {
-            ItemBaseInfoDetail?.ItemImageUrl1 &&   <Col span={6} className="imgContent">
-            <img
-              src={ ItemBaseInfoDetail?.ItemImageUrl1}
-              alt=""
-            />
-          </Col>
-          }
-          {
-            ItemBaseInfoDetail?.ItemImageUrl2 &&   <Col span={6} className="imgContent">
-            <img
-              src={ ItemBaseInfoDetail?.ItemImageUrl2}
-              alt=""
-            />
-          </Col>
-          }
-        </Row>
-        <div className="footer">
-          <SectionTitle title="Delivery Packing Specification List" />
-        
-          <Tabs defaultActiveKey="1" onChange={callback} className="tabs">
-            {
-              ItemPackingSpecificationDetails.map((item,index) =>   <TabPane tab={`Tab ${index+1}`} key={item.TransactionNumber}>
-             
-              {tabsView(item,getTypeName(Item_Packing_Type,item.PackingTypeCode))}
-            </TabPane> )
-            }
-       
-          </Tabs>
-              {/* log */}
-              <SectionTitle title="Log" />
-          <Row>
-            <Col span={24}  style={{ textAlign:'right',marginTop:'1px',marginBottom:'12px' }} className="log-header">
-               <a onClick={openLog}>Expand log {checkLog ? <DownOutlined  /> : <RightOutlined />}</a>
-            </Col>
-            <Col span={24} className="log-list">
-             
-              {
-                logList.map((item,key) => <div className={` header-card`} style={{ padding:'8px 0' }} key={item.TransactionNumber}>
-                <div className="table-status-wrap flex-sc">
-                  <span className={`status blue`} style={{ background: '#0072EC' }}></span>
-              <span>{getTypeName(Item_OperationLog_Type,item.OperationCode)}   <span style={{  fontWeight:'700' }}>{item?.Memo}</span> {item?.Operator}  {moment(item?.OperationTime).format('YYYY-MM-DD hh:mm:ss')} </span>
-                </div>
-              </div>)
-              }
-            </Col>
-          </Row>
-          {
-              [2].includes(data.ItemBaseInfoDetail.Status) && <Row>
-           
-              <Col span={12} offset={12} style={{ textAlign: "right" }}>
-                <Button
-                  style={{ marginLeft: 20 }}
-                  icon={<DelsSvg className="anticon" />}
-                  onClick={() => rejected()}
-                >
-                  REJECTED
-                </Button>
-  
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  style={{ marginLeft: 20 }}
-                  icon={<OKSvg className="anticon" />}
-                  onClick={() => approve()}
-                >
-                  APPROVE
-                </Button>
-              </Col>
-            </Row>
-            }
-              
-        
+  const del = index => {
+    data.splice(index, 1)
+    setData([...data])
+  }
+  const changeInput = (e, index) => {
+    data[index].value = e.target.value
+    setData([...data])
+  }
+  const save = index => {
+    data[index].isEdit = false
+    setData([...data])
+  }
+
+  const setColor = index => {
+    data[index].isColor = true
+    setData([...data])
+  }
+
+  const getIssueViewCopy = () => {
+    const _data = data.filter(item => !item.isEdit)
+    _data.map(item => (item.isCheckAnswer = false))
+    setData2([..._data])
+  }
+
+  const getAnswer = index => {
+    data2[index].isCheckAnswer = !data2[index].isCheckAnswer
+    const _data = data2.filter(item => item.isCheckAnswer)
+    setData2([...data2])
+    setData3([..._data])
+  }
+  const getView = (item, index) => {
+    let _isColor = item.isColor && 'isColor'
+    if (item.isEdit) {
+      return (
+        <div className="item-div-input-content  isEdit" key={index}>
+          <Input
+            placeholder="Borderless"
+            maxLength={20}
+            bordered={false}
+            onChange={e => {
+              changeInput(e, index)
+            }}
+          />
+
+          <CheckCircleOutlined
+            onClick={() => {
+              del(index)
+            }}
+          />
+          <CheckCircleOutlined
+            onClick={() => {
+              save(index)
+            }}
+          />
+          <CheckCircleOutlined
+            onClick={() => {
+              setColor(index)
+            }}
+          />
         </div>
-
-        <Modal
-          title={"Rejected"}
-          width={800}
-          wrapClassName="serice-modal"
-          visible={visible}
-          destroyOnClose={true}
-          onCancel={() => handleCancel()}
-          footer={[
-            <Button
-              key="submit"
-              type="primary"
-              icon={<OKSvg className="anticon" />}
-              onClick={() => handleOk()}
-            >
-              CONFIRM
-            </Button>,
-          ]}
+      )
+    } else {
+      return (
+        <div
+          className={`item-div-input-content isNotEdit ${_isColor}`}
+          key={index}
         >
-          <Form form={form} layout="vertical">
-            <Row gutter={20}>
-              <Col span={24}>
-                <Form.Item
-                  label="Reason for Rejection :"
-                  name="Notice"
-                  rules={[
-                    { required: true, message: "please enter!" },
-                    {
-                      pattern: /[^\s]/,
-                      message: "Do not enter Spaces!",
-                    },
-                  ]}
-                >
-                  <TextArea rows={2} placeholder="please enter..." />
-                </Form.Item>
-              </Col>
-            </Row>
-          </Form>
-        </Modal>
-      </div>
-    </Page>
-  );
-};
+          {item.value}
+        </div>
+      )
+    }
+  }
 
-export default withRouter(Loading(PackingDetail));
+  const getIssueView = (item, index) => {
+    let _isColor = item.isColor && 'isColor'
+    let _isCheckAnswer = item.isCheckAnswer && 'isCheckAnswer'
+    return (
+      <div
+        className={`item-div-input-content isNotEdit ${_isColor}  ${_isCheckAnswer}`}
+        key={index}
+        onClick={() => {
+          getAnswer(index)
+        }}
+      >
+        {item.value}
+      </div>
+    )
+  }
+
+  const getDisturbView = (item, index) => {
+    if (item.isEdit) {
+      return (
+        <div className="item-div-input-content  isEdit" key={index}>
+          <Input
+            placeholder="Borderless"
+            maxLength={20}
+            bordered={false}
+            onChange={e => {
+              data4[index].value = e.target.value
+              setData4([...data4])
+            }}
+          />
+
+          <CheckCircleOutlined
+            onClick={() => {
+              data4.splice(index, 1)
+              setData4([...data4])
+            }}
+          />
+          <CheckCircleOutlined
+            onClick={() => {
+              data4[index].isEdit = false
+              setData4([...data4])
+            }}
+          />
+        </div>
+      )
+    } else {
+      return (
+        <div className={`item-div-input-content isNotEdit`} key={index}>
+          {item.value}
+        </div>
+      )
+    }
+  }
+  return (
+    <>
+      <Card
+        title={title}
+        type="inner"
+        className="fe-card"
+        extra={
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a href="#" alt="">
+            More
+          </a>
+        }
+        style={{ width: '400px' }}
+      >
+        <Row gutter={20} className="card-content">
+          <Col span={24} className="item">
+            <label>xx:</label>
+            <div className="item-div clearfix">
+              {data.map((item, index) => getView(item, index))}
+            </div>
+            <StarTwoTone onClick={setPushData} />
+          </Col>
+
+          <Col span={24} className="item">
+            <label>xx:</label>
+            <div className="item-div clearfix">
+              {data2.map((item, index) => getIssueView(item, index))}
+            </div>
+            <UpSquareOutlined onClick={getIssueViewCopy} />
+          </Col>
+
+          <Col span={24} className="item">
+            <label>xx:</label>
+            <div className="item-div clearfix">
+              {data3.map((item, index) => getIssueView(item, index))}
+            </div>
+            <span className="br"></span>
+          </Col>
+
+          <Col span={24} className="item">
+            <label>xx:</label>
+            <div className="item-div clearfix">
+              {data4.map((item, index) => getDisturbView(item, index))}
+            </div>
+            <StarTwoTone onClick={setDisturbData} />
+          </Col>
+        </Row>
+      </Card>
+    </>
+  )
+}
+export default MyCard
