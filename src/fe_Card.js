2a51db73922a58f695ee9b4567152342cd2a0d6c
童一 <tongwenfan1991@yahoo.com>
2021-12-08 17:04:32 +0800

    Update fe_Card.js

--- src/fe_Card.js
+++ src/fe_Card.js
@@ -1,236 +1,181 @@
 import React, { useEffect, useState } from 'react'
-import { Row, Col, Card, Input } from 'antd'
+import { Row, Col, Card, Checkbox, Table } from 'antd'
 import './App.less'
 import {
-  StarTwoTone,
-  CheckCircleOutlined,
-  UpSquareOutlined,
+  ArrowUpOutlined,
+  ArrowDownOutlined,
+  DeleteOutlined,
+  NotificationTwoTone,
 } from '@ant-design/icons'
-const MyCard = ({ title = '2222' }) => {
-  const [data, setData] = useState([])
-  const [data2, setData2] = useState([
-    // isEdit: true,
-    // value: '',
-    // isColor: false,
-    // isCheckAnswer:false
-  ])
-  const [data3, setData3] = useState([])
-  const [data4, setData4] = useState([])
-  // 添加词块
-  const setPushData = () => {
-    if (data.length > 7) return
-    data.push({
-      isEdit: true,
-      value: '',
-      isColor: false,
-    })
-    setData([...data])
-  }
-  // 添加干扰
-  const setDisturbData = () => {
-    if (data4.length > 7) return
-    data4.push({
-      isEdit: true,
-      value: '',
-      isColor: false,
-    })
-    setData4([...data4])
+const MyCard = () => {
+  function onChange(e) {
+    console.log(`checked = ${e.target.checked}`)
   }
 
-  const del = index => {
-    data.splice(index, 1)
-    setData([...data])
-  }
-  const changeInput = (e, index) => {
-    data[index].value = e.target.value
-    setData([...data])
-  }
-  const save = index => {
-    data[index].isEdit = false
-    setData([...data])
-  }
+  const dataSource = [
+    {
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号',
+    },
+    {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号',
+    },
+  ]
 
-  const setIsEdit = index => {
-    data[index].isEdit = true
-    setData([...data])
-  }
-  const setIsEdit2 = index => {
-    data4[index].isEdit = true
-    setData4([...data4])
-  }
+  const columns = [
+    {
+      title: '序号',
+      dataIndex: 'key',
+      key: 'key',
+      ellipsis: true,
+      width: 80,
+    },
+    {
+      title: '操作',
+      dataIndex: 'age',
+      key: 'age',
 
-  const setColor = index => {
-    data[index].isColor = true
-    setData([...data])
-  }
-
-  const getIssueViewCopy = () => {
-    const _data = data.filter(item => !item.isEdit)
-    _data.map(item => (item.isCheckAnswer = false))
-    setData2([..._data])
-  }
-
-  const getAnswer = index => {
-    data2[index].isCheckAnswer = !data2[index].isCheckAnswer
-    const _data = data2.filter(item => item.isCheckAnswer)
-    setData2([...data2])
-    setData3([..._data])
-  }
-  const getView = (item, index) => {
-    let _isColor = item.isColor && 'isColor'
-    if (item.isEdit) {
-      return (
-        <div className="item-div-input-content  isEdit" key={index}>
-          <Input
-            placeholder="Borderless"
-            maxLength={7}
-            value={item.value}
-            bordered={false}
-            onChange={e => {
-              changeInput(e, index)
-            }}
-          />
-
-          <CheckCircleOutlined
-            onClick={() => {
-              del(index)
-            }}
-          />
-          <CheckCircleOutlined
-            onClick={() => {
-              save(index)
-            }}
-          />
-          <CheckCircleOutlined
-            onClick={() => {
-              setColor(index)
-            }}
-          />
+      width: 120,
+      render: () => (
+        <div>
+          <ArrowUpOutlined /> <ArrowDownOutlined style={{ margin: '0 12px' }} />
+          <DeleteOutlined />
         </div>
-      )
-    } else {
-      return (
+      ),
+    },
+    {
+      title: '单词',
+      dataIndex: 'address',
+      key: 'address',
+
+      width: 150,
+      render: () => (
+        <div>
+          <NotificationTwoTone /> xxxxx
+        </div>
+      ),
+    },
+    {
+      title: '词义',
+      dataIndex: 'address',
+      key: 'address',
+      width: 200,
+      render: () => (
+        <div>
+          <p>
+            <Checkbox onChange={onChange}>Checkbox</Checkbox>
+          </p>
+          <p>
+            <Checkbox onChange={onChange}>Checkbox</Checkbox>
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: '配图',
+      dataIndex: 'address',
+      key: 'address',
+      render: () => (
         <div
-          className={`item-div-input-content isNotEdit ${_isColor}`}
-          key={index}
-          onClick={() => {
-            setIsEdit(index)
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignContent: 'space-between',
           }}
         >
-          {item.value}
-        </div>
-      )
-    }
-  }
-
-  const getIssueView = (item, index) => {
-    let _isColor = item.isColor && 'isColor'
-    let _isCheckAnswer = item.isCheckAnswer && 'isCheckAnswer'
-    return (
-      <div
-        className={`item-div-input-content isNotEdit ${_isColor}  ${_isCheckAnswer}`}
-        key={index}
-        onClick={() => {
-          getAnswer(index)
-        }}
-      >
-        {item.value}
-      </div>
-    )
-  }
-
-  const getDisturbView = (item, index) => {
-    if (item.isEdit) {
-      return (
-        <div className="item-div-input-content  isEdit" key={index}>
-          <Input
-            placeholder="Borderless"
-            maxLength={7}
-            bordered={false}
-            value={item.value}
-            onChange={e => {
-              data4[index].value = e.target.value
-              setData4([...data4])
+          <img
+            src="https://dfis-gdev.newegg.org/globalselling/quote/dfd7d690-daf7-4e9c-ae30-24902e844efc.jpg"
+            style={{
+              maxWidth: '50%',
+              maxHeight: '130px',
+              margin: ' auto',
+              padding: '6px',
             }}
+            alt=""
           />
-
-          <CheckCircleOutlined
-            onClick={() => {
-              data4.splice(index, 1)
-              setData4([...data4])
+          <img
+            src="https://s0.2mdn.net/simgad/13643478230671096814?sqp=-oaymwEOCKwCEPoBIAFIZFABWAE&rs=AOga4qmJpdn4M7kLS98kIVh_mkNkTpKm7w"
+            style={{
+              maxWidth: '50%',
+              maxHeight: '130px',
+              margin: ' auto',
+              padding: '6px',
             }}
+            alt=""
           />
-          <CheckCircleOutlined
-            onClick={() => {
-              data4[index].isEdit = false
-              setData4([...data4])
+          <img
+            src="https://s2.2mdn.net/proxy/HPCfHJbK08QtjbjI6CxuVuEcZ_tJqh8uaL2eHldYPADijh8On27ySYzsLodid_0Is6MMyScXC6KvCxpeKpGFUeHIhwHaYSuzhgsEdGm2qL54MMDZistNJv2xZ4LhwSnWrHivenNbHCfO=w910-h112-n"
+            style={{
+              maxWidth: '50%',
+              maxHeight: '130px',
+              margin: ' auto',
+              padding: '6px',
             }}
+            alt=""
+          />
+          <img
+            src="https://s0.2mdn.net/simgad/13643478230671096814?sqp=-oaymwEOCKwCEPoBIAFIZFABWAE&rs=AOga4qmJpdn4M7kLS98kIVh_mkNkTpKm7w"
+            style={{
+              maxWidth: '50%',
+              maxHeight: '130px',
+              margin: ' auto',
+              padding: '6px',
+            }}
+            alt=""
           />
         </div>
-      )
-    } else {
-      return (
-        <div
-          className={`item-div-input-content isNotEdit`}
-          onClick={() => {
-            setIsEdit2(index)
-          }}
-          key={index}
-        >
-          {item.value}
+      ),
+    },
+    {
+      title: 'x1',
+      dataIndex: 'address',
+      key: 'address',
+      width: 80,
+      render: () => (
+        <div>
+          <Checkbox onChange={onChange}></Checkbox>
         </div>
-      )
-    }
-  }
+      ),
+    },
+    {
+      title: 'x2',
+      dataIndex: 'address',
+      key: 'address',
+      width: 80,
+      render: () => (
+        <div>
+          <Checkbox onChange={onChange}></Checkbox>
+        </div>
+      ),
+    },
+    {
+      title: 'x3',
+      dataIndex: 'address',
+      key: 'address',
+      ellipsis: true,
+      width: 80,
+      render: () => (
+        <div>
+          <Checkbox onChange={onChange}></Checkbox>
+        </div>
+      ),
+    },
+  ]
   return (
-    <>
-      <Card
-        title={title}
-        type="inner"
-        className="fe-card"
-        extra={
-          // eslint-disable-next-line jsx-a11y/anchor-is-valid
-          <a href="#" alt="">
-            More
-          </a>
-        }
-        style={{ width: '400px' }}
-      >
-        <Row gutter={20} className="card-content">
-          <Col span={24} className="item">
-            <label>xx:</label>
-            <div className="item-div clearfix">
-              {data.map((item, index) => getView(item, index))}
-            </div>
-            <StarTwoTone onClick={setPushData} />
-          </Col>
-
-          <Col span={24} className="item">
-            <label>xx:</label>
-            <div className="item-div clearfix">
-              {data2.map((item, index) => getIssueView(item, index))}
-            </div>
-            <UpSquareOutlined onClick={getIssueViewCopy} />
-          </Col>
-
-          <Col span={24} className="item">
-            <label>xx:</label>
-            <div className="item-div clearfix">
-              {data3.map((item, index) => getIssueView(item, index))}
-            </div>
-            <span className="br"></span>
-          </Col>
-
-          <Col span={24} className="item">
-            <label>xx:</label>
-            <div className="item-div clearfix">
-              {data4.map((item, index) => getDisturbView(item, index))}
-            </div>
-            <StarTwoTone onClick={setDisturbData} />
-          </Col>
-        </Row>
-      </Card>
-    </>
+    <div>
+      <Table
+        pagination={false}
+        scroll={{ x: 1200 }}
+        dataSource={dataSource}
+        bordered
+        columns={columns}
+      />
+    </div>
   )
 }
 export default MyCard
